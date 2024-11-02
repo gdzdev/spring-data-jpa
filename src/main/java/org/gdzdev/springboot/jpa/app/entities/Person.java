9dcd1d76887bac8cb8ea63dc96f10f32fea3f406
Aleks Galdamez <aleksgaldamez2002@gmail.com>
2024-11-02 11:05:07 -0600

    add class entity

---
+++ src/main/java/org/gdzdev/springboot/jpa/app/entities/Person.java
@@ -0,0 +1,26 @@
+package org.gdzdev.springboot.jpa.app.entities;
+
+import lombok.*;
+import jakarta.persistence.*;
+
+@Data
+@Entity
+@Builder
+@Table(name = "persons")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "programming_language")
+    private String programmingLanguage;
+}
