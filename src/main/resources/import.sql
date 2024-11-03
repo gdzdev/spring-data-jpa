310635b74608ec73d53165b39df97f87e354c780
Aleks Galdamez <aleksgaldamez2002@gmail.com>
2024-11-03 15:00:08 -0600

    added sample data in import.sql for testing

---
+++ src/main/resources/import.sql
@@ -0,0 +1,13 @@
+INSERT INTO persons (first_name, last_name, programming_language) VALUES ('Lucy', 'Galdamez', 'Java');
+INSERT INTO persons (first_name, last_name, programming_language) VALUES ('Aleks', 'Galdamez', 'Python');
+INSERT INTO persons (first_name, last_name, programming_language) VALUES ('Yoko', 'Velilla', 'JavaScript');
+INSERT INTO persons (first_name, last_name, programming_language) VALUES ('Luna', 'Fuentes', 'C#');
+INSERT INTO persons (first_name, last_name, programming_language) VALUES ('Candy', 'Melgar', 'Cobol');
+
+INSERT INTO users (username, email, password) VALUES ('Black', 'Ghost@gmail.com', '1234');
+INSERT INTO users (username, email, password) VALUES ('Lunita', 'lunita@gmail.com', '1234');
+INSERT INTO users (username, email, password) VALUES ('Deus', 'deus@gmail.com', '1234');
+INSERT INTO users (username, email, password) VALUES ('Deus', 'deus2000@gmail.com', '1234');
+INSERT INTO users (username, email, password) VALUES ('Candy', 'Candy@gmail.com', '2222');
+INSERT INTO users (username, email, password) VALUES ('Cristal', 'cristal@gmail.com', '1234');
+
