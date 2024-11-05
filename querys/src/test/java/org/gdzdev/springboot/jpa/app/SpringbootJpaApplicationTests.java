3241975feb4826c0b99433a52c3994c64f32d5b3
Aleks Galdamez <aleksgaldamez2002@gmail.com>
2024-11-04 21:46:32 -0600

    remaned

---
+++ querys/src/test/java/org/gdzdev/springboot/jpa/app/SpringbootJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gdzdev.springboot.jpa.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
